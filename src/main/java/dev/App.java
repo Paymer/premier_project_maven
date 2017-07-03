4a0acca066383df8ecf54f4073928d9d75b4f509
Paula Aymerich <p.aymerich.f@gmail.com>
2017-07-03 14:21:08 +0200

    5 Commit

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,14 +1,25 @@
 package dev;
 
+
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  *
  */
 public class App 
+
+
 {
     public static void main( String[] args )
     {
 
         System.out.println( "Hello World!" );
+        String asciiArt = FigletFont.convertOneLine("olalala");
+        System.out.println(asciiArt);
     }
 }
+
+
+
+
